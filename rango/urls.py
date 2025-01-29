b502cfb276a2630cd7ae95716e215cf11d4696d2
Nelson Hearfield <2884923h@student.gla.ac.uk>
2025-01-29 13:44:44 +0000

    chapters 3 and 4

--- rango/urls.py
+++ rango/urls.py
@@ -5,4 +5,6 @@ app_name = 'rango'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("Rango says hi!")
+def index(request): 
+    # return HttpResponse("<a href='/rango/about'>Rango says hey there partner!</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, cnady, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
 
+def about(request):
+    # return HttpResponse("<a href='/rango/index'>Rango says here is the about page.</a>")
+    context_dict = {'boldmessage': 'This tutorial has been created by Nelson.'}
+    return render(request, 'rango/about.html', context=context_dict)
