84a9879447b77c7b661881f57cb2b42704af7272
Nelson Hearfield <2884923h@student.gla.ac.uk>
2025-02-11 12:56:39 +0000

    chapter 9

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from rango.models import Category, Page
+from rango.models import Category, Page, UserProfile
 
 # class PageInline(admin.TabularInline):
 #     model = Page
@@ -24,3 +24,4 @@ class CategoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
